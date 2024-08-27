9cc88f9a09f5327ee7772d457546e64fe5c42188
Isolumi <hubertx98@gmail.com>
2024-08-27 17:37:02 -0400

    fixed ssl issues

--- run.py
+++ run.py
@@ -1,4 +1,25 @@
-from services import data_access
+from pymongo.mongo_client import MongoClient
+from urllib.parse import quote_plus
+import certifi
+import os
+from dotenv import load_dotenv
 
-drug = data_access.test()
-data_access.add_item(drug)
+load_dotenv()
+
+
+password = quote_plus(os.getenv('MONGO_PASS'))
+passs = quote_plus('1@XDaumisomDX')
+print(password)
+print(passs)
+uri = 'mongodb+srv://isolumi:' + passs + '@medicine-organizer.yq45ftm.mongodb.net/?retryWrites=true&w=majority&appName=medicine-organizer'
+
+
+# Create a new client and connect to the server
+client = MongoClient(uri, tlsCAFile=certifi.where())
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
--- services/__init__.py
+++ services/__init__.py
@@ -1,7 +1,8 @@
 import os
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
 from dotenv import load_dotenv
 from urllib.parse import quote_plus
+import certifi
 
 load_dotenv()
 
@@ -9,10 +10,7 @@ class MongoDB:
     def __init__(self):
         password = quote_plus(os.getenv('MONGO_PASS'))
         uri = 'mongodb+srv://isolumi:' + password + '@medicine-organizer.yq45ftm.mongodb.net/?retryWrites=true&w=majority&appName=medicine-organizer'
-        self.client = MongoClient(uri)
-        self.db = self.client['medication-organiser']
-
-        # self.db = self.client[db_name]
-        
+        self.client = MongoClient(uri, tlsCAFile=certifi.where())
+        self.db = self.client['medication-organiser']      
         
 mongo = MongoDB()
